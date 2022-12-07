b7a70081e0cd0b03c9cd39cc120c58daaab5a0de
ShamaKung <shamaturin@gmail.com>
2022-12-07 17:43:37 +0500

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите 5-ти значное число");
+string a = Console.ReadLine();
+int alength = a.Length;
+
+if(alength == 5)
+{
+    char[] revA = a.ToCharArray();
+    Array.Reverse(revA);
+    string backCurrens = new string(revA);
+    int resultPolindrom = Convert.ToInt32(a) - Convert.ToInt32(backCurrens);
+    if(resultPolindrom == 0)
+    {
+        Console.WriteLine("Полиндром");
+    }
+    else
+    {
+        Console.WriteLine("Не яв-ся полиндромом");
+    }
+}
+else
+{
+    Console.WriteLine("не верный формат числа");
+}
