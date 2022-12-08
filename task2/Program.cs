e76851e8d73ec4a929e086444a2ecbe91f646bbb
ShamaKung <shamaturin@gmail.com>
2022-12-08 11:56:35 +0500

    правки

--- task2/Program.cs
+++ task2/Program.cs
@@ -14,5 +14,5 @@ for(int i = 0; i < B.Length; i++)
     B[i] = int.Parse(Console.ReadLine());
 }
 
-double sum = Math.Sqrt(Math.Pow(A[0]-B[0],2)+Math.Pow(A[1]-B[1],2)+Math.Pow(A[2]-B[2],2));
+double sum = Math.Sqrt(Math.Pow(A[0] - B[0],2) + Math.Pow(A[1] - B[1],2) + Math.Pow(A[2] - B[2],2));
 Console.WriteLine("Растояние между точками " + sum);
