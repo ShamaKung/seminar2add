946fb801947b7dd099268ceeda07de922bb2618a
ShamaKung <shamaturin@gmail.com>
2022-12-08 12:37:43 +0500

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿void Cube (int[] arr, int len)
+{
+    for(int i=0; i < len; i++)
+    {
+        double cubeNumber = Math.Pow(arr[i],3);
+        Console.WriteLine(cubeNumber);
+    }
+}
+
+
+Console.WriteLine("Введите число");
+int lengthA = int.Parse(Console.ReadLine());
+int[] A = new int[lengthA];
+
+for(int i = 0; i < A.Length; i++)
+{
+    A[i] = i + 1;
+}
+
+Cube(A, lengthA);
